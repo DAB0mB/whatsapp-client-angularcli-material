Urigo/WhatsApp-Clone-Client-Angular

Step 8.1: New chats and groups

src/app/chats-creation/components/users-list/users-list.component.ts
@@ -1,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {GetUsers} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
+
+@Component({
+  selector: 'app-users-list',
+  template: `
+    <mat-list>
+      <mat-list-item *ngFor="let user of users">
+        <app-user-item [item]="user"
+                       appSelectableItem></app-user-item>
+      </mat-list-item>
+    </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
+  `,
+  styleUrls: ['users-list.component.scss'],
+})
+export class UsersListComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('items')
+  users: GetUsers.Users[];
+
+  constructor(public selectableListDirective: SelectableListDirective) {}
+}

src/app/chats-creation/containers/new-chat/new-chat.component.ts
@@ -1,0 +1,59 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {AddChat, GetUsers} from '../../../../types';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goBack()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <div class="title">New chat</div>
+    </app-toolbar>
+
+    <div class="new-group" (click)="goToNewGroup()">
+      <div>
+        <mat-icon aria-label="Icon-button with a group add icon">group_add</mat-icon>
+      </div>
+      <div>New group</div>
+    </div>
+
+    <app-users-list [items]="users"
+                    appSelectableList="single" (single)="addChat($event)">
+    </app-users-list>
+  `,
+  styleUrls: ['new-chat.component.scss'],
+})
+export class NewChatComponent implements OnInit {
+  users: GetUsers.Users[];
+
+  constructor(private router: Router,
+              private location: Location,
+              private chatsService: ChatsService) {}
+
+  ngOnInit () {
+    this.chatsService.getUsers().users$.subscribe(users => this.users = users);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  goToNewGroup() {
+    this.router.navigate(['/new-group']);
+  }
+
+  addChat(recipientId: string) {
+    const chatId = this.chatsService.getChatId(recipientId);
+    if (chatId) {
+      // Chat is already listed for the current user
+      this.router.navigate(['/chat', chatId]);
+    } else {
+      this.chatsService.addChat(recipientId).subscribe(({data: {addChat: {id}}}: { data: AddChat.Mutation }) => {
+        this.router.navigate(['/chat', id]);
+      });
+    }
+  }
+}

src/app/chats-lister/containers/chats/chats.component.ts
@@ -34,7 +34,7 @@ import {Router} from '@angular/router';
       <app-confirm-selection #confirmSelection></app-confirm-selection>
     </app-chats-list>
 
-    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="primary">
+    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="primary" (click)="goToNewChat()">
       <mat-icon aria-label="Icon-button with a + icon">add</mat-icon>
     </button>
   `,
@@ -54,6 +54,10 @@ export class ChatsComponent implements OnInit {
 
   goToChat(chatId: string) {
     this.router.navigate(['/chat', chatId]);
+  }
+
+  goToNewChat() {
+    this.router.navigate(['/new-chat']);
   }
 
   deleteChats(chatIds: string[]) {

DAB0mB/whatsapp-client-angularcli-material

src/app/chats-creation/components/users-list/users-list.component.ts
@@ -1,0 +1,25 @@
+import {Component, Input} from '@angular/core';
+import {GetUsers} from '../../../../graphql';
+import {SelectableListDirective} from 'ngx-selectable-list';
+
+@Component({
+  selector: 'app-users-list',
+  template: `
+    <mat-list>
+      <mat-list-item *ngFor="let user of users">
+        <app-user-item [item]="user"
+                       [selected]="selectableListDirective.selectedItemIds.includes(user.id)"
+                       libSelectableItem></app-user-item>
+      </mat-list-item>
+    </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
+  `,
+  styleUrls: ['users-list.component.scss'],
+})
+export class UsersListComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('items')
+  users: GetUsers.Users[];
+
+  constructor(public selectableListDirective: SelectableListDirective) {}
+}

src/app/chats-creation/containers/new-chat/new-chat.component.ts
@@ -1,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {AddChat, GetUsers} from '../../../../graphql';
+import {ChatsService} from '../../../services/chats.service';
+
+@Component({
+  template: `
+    <app-toolbar>
+      <button class="navigation" mat-button (click)="goBack()">
+        <mat-icon aria-label="Icon-button with an arrow back icon">arrow_back</mat-icon>
+      </button>
+      <div class="title">New chat</div>
+    </app-toolbar>
+    <div class="new-group" (click)="goToNewGroup()">
+      <mat-icon color="secondary" aria-label="Icon-button with a group add icon">group_add</mat-icon>
+      <div>New group</div>
+    </div>
+    <app-users-list [items]="users"
+                    libSelectableList="single" (single)="addChat($event)">
+    </app-users-list>
+  `,
+  styleUrls: ['new-chat.component.scss'],
+})
+export class NewChatComponent implements OnInit {
+  users: GetUsers.Users[];
+
+  constructor(private router: Router,
+              private location: Location,
+              private chatsService: ChatsService) {}
+
+  ngOnInit () {
+    this.chatsService.getUsers().users$.subscribe(users => this.users = users);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  goToNewGroup() {
+    this.router.navigate(['/new-group']);
+  }
+
+  addChat(recipientId: string) {
+    const chatId = this.chatsService.getChatId(recipientId);
+    if (chatId) {
+      // Chat is already listed for the current user
+      this.router.navigate(['/chat', chatId]);
+    } else {
+      this.chatsService.addChat(recipientId).subscribe(({data: {addChat: {id}}}: { data: AddChat.Mutation }) => {
+        this.router.navigate(['/chat', id]);
+      });
+    }
+  }
+}

src/app/chats-lister/containers/chats/chats.component.ts
@@ -34,7 +34,7 @@ import {Router} from '@angular/router';
       <app-confirm-selection #confirmSelection></app-confirm-selection>
     </app-chats-list>
 
-    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="secondary">
+    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="secondary" (click)="goToNewChat()">
       <mat-icon aria-label="Icon-button with a + icon">chat</mat-icon>
     </button>
   `,
@@ -54,6 +54,10 @@ export class ChatsComponent implements OnInit {
 
   goToChat(chatId: string) {
     this.router.navigate(['/chat', chatId]);
+  }
+
+  goToNewChat() {
+    this.router.navigate(['/new-chat']);
   }
 
   deleteChats(chatIds: string[]) {