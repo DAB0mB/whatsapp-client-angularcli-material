Urigo/WhatsApp-Clone-Client-Angular

Step 4.2: Add generated types

src/app/chat-viewer/components/message-item/message-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../types';
 
 @Component({
   selector: 'app-message-item',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class MessageItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
-  message: any;
+  message: GetChat.Messages;
 
   @Input()
   isGroup: boolean;

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ChatsService} from '../../../services/chats.service';
+import {GetChat} from '../../../../types';
 
 @Component({
   template: `
@@ -19,7 +20,7 @@ import {ChatsService} from '../../../services/chats.service';
 })
 export class ChatComponent implements OnInit {
   chatId: string;
-  messages: any[];
+  messages: GetChat.Messages[];
   name: string;
   isGroup: boolean;
 

DAB0mB/whatsapp-client-angularcli-material

src/app/chat-viewer/components/message-item/message-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChat} from '../../../../graphql';
 
 @Component({
   selector: 'app-message-item',
@@ -15,7 +16,7 @@ import {Component, Input} from '@angular/core';
 export class MessageItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
-  message: any;
+  message: GetChat.Messages;
 
   @Input()
   isGroup: boolean;

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ChatsService} from '../../../services/chats.service';
+import {GetChat} from '../../../../graphql';
 
 @Component({
   template: `
@@ -20,7 +21,7 @@ import {ChatsService} from '../../../services/chats.service';
 })
 export class ChatComponent implements OnInit {
   chatId: string;
-  messages: any[];
+  messages: GetChat.Messages[];
   name: string;
   isGroup: boolean;
 