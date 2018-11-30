Urigo/WhatsApp-Clone-Client-Angular

Step 4.1: Chat Viewer

src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {GetChats} from '../../../../types';
 
 @Component({
   selector: 'app-chat-item',
   template: `
     <div class="chat-row">
-        <div class="chat-recipient">
+        <div class="chat-recipient" (click)="selectChat()">
           <img *ngIf="chat.picture" [src]="chat.picture" width="48" height="48">
           <div>{{ chat.name }} [id: {{ chat.id }}]</div>
         </div>
@@ -18,4 +18,11 @@ export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
   chat: GetChats.Chats;
+
+  @Output()
+  select = new EventEmitter<string>();
+
+  selectChat() {
+    this.select.emit(this.chat.id);
+  }
 }

DAB0mB/whatsapp-client-angularcli-material

src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {GetChats} from '../../../../graphql';
 
 @Component({
   selector: 'app-chat-item',
   template: `
-    <div class="chat-row">
+    <div class="chat-row" (click)="selectChat()">
       <img class="chat-pic" [src]="chat.picture || 'assets/default-profile-pic.jpg'">
       <div class="chat-info">
         <div class="chat-name">{{ chat.name }}</div>
@@ -19,4 +19,11 @@ export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
   chat: GetChats.Chats;
+
+  @Output()
+  select = new EventEmitter<string>();
+
+  selectChat() {
+    this.select.emit(this.chat.id);
+  }
 }