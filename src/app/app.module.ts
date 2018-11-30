Urigo/WhatsApp-Clone-Client-Angular

Step 10.1: Authentication

src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {HttpLink, HttpLinkModule, Options} from 'apollo-angular-link-http';
 import {Apollo, ApolloModule} from 'apollo-angular';
 import {defaultDataIdFromObject, InMemoryCache} from 'apollo-cache-inmemory';
@@ -11,10 +11,10 @@ import {ChatsListerModule} from './chats-lister/chats-lister.module';
 import {RouterModule, Routes} from '@angular/router';
 import {ChatViewerModule} from './chat-viewer/chat-viewer.module';
 import {ChatsCreationModule} from './chats-creation/chats-creation.module';
+import {LoginModule} from './login/login.module';
+import {AuthInterceptor} from './login/services/auth.interceptor';
 
-const routes: Routes = [
-  {path: '', redirectTo: 'chats', pathMatch: 'full'},
-];
+const routes: Routes = [];
 
 @NgModule({
   declarations: [
@@ -32,8 +32,15 @@ const routes: Routes = [
     ChatsListerModule,
     ChatViewerModule,
     ChatsCreationModule,
+    LoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {

src/app/login/services/login.service.ts
@@ -1,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {User} from '../../../types';
+
+@Injectable()
+export class LoginService {
+
+  constructor() { }
+
+  storeAuthHeader(auth: string) {
+    localStorage.setItem('Authorization', auth);
+  }
+
+  getAuthHeader(): string {
+    return localStorage.getItem('Authorization');
+  }
+
+  storeUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUser(): User {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+}

DAB0mB/whatsapp-client-angularcli-material

src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { GraphQLModule } from './graphql.module';
 import {ChatsListerModule} from './chats-lister/chats-lister.module';
 import {RouterModule, Routes} from '@angular/router';
 import {ChatViewerModule} from './chat-viewer/chat-viewer.module';
 import {ChatsCreationModule} from './chats-creation/chats-creation.module';
+import {LoginModule} from './login/login.module';
+import {AuthInterceptor} from './login/services/auth.interceptor';
 const routes: Routes = [];
 
 @NgModule({
@@ -24,8 +26,15 @@ const routes: Routes = [];
     ChatsListerModule,
     ChatViewerModule,
     ChatsCreationModule,
+    LoginModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

src/app/login/services/login.service.ts
@@ -1,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {User} from '../../../graphql';
+
+@Injectable()
+export class LoginService {
+
+  constructor() { }
+
+  storeAuthHeader(auth: string) {
+    localStorage.setItem('Authorization', auth);
+  }
+
+  getAuthHeader(): string {
+    return localStorage.getItem('Authorization');
+  }
+
+  storeUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUser(): User {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+}