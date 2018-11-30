Urigo/WhatsApp-Clone-Client-Angular

Step 2.3: Use the generated types

src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChats} from '../../../../types';
 
 @Component({
   selector: 'app-chat-item',
@@ -16,5 +17,5 @@ import {Component, Input} from '@angular/core';
 export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
-  chat: any;
+  chat: GetChats.Chats;
 }

src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChats} from '../../../../types';
 
 @Component({
   selector: 'app-chats-list',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class ChatsListComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('items')
-  chats: any[];
+  chats: GetChats.Chats[];
 
   constructor() {}
 }

src/app/chats-lister/containers/chats/chats.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ChatsService} from '../../../services/chats.service';
 import {Observable} from 'rxjs/Observable';
+import {GetChats} from '../../../../types';
 
 @Component({
   template: `
@@ -35,7 +36,7 @@ import {Observable} from 'rxjs/Observable';
   styleUrls: ['./chats.component.scss'],
 })
 export class ChatsComponent implements OnInit {
-  chats$: Observable<any[]>;
+  chats$: Observable<GetChats.Chats[]>;
 
   constructor(private chatsService: ChatsService) {
   }

src/app/services/chats.service.ts
@@ -3,6 +3,7 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
+import {GetChats} from '../../types';
 
 @Injectable()
 export class ChatsService {
@@ -10,11 +11,11 @@ export class ChatsService {
   constructor(private apollo: Apollo) {}
 
   getChats() {
-    const query = this.apollo.watchQuery<any>({
+    const query = this.apollo.watchQuery<GetChats.Query>({
       query: getChatsQuery
     });
     const chats$ = query.valueChanges.pipe(
-      map((result: ApolloQueryResult<any>) => result.data.chats)
+      map((result: ApolloQueryResult<GetChats.Query>) => result.data.chats)
     );
 
     return {query, chats$};

DAB0mB/whatsapp-client-angularcli-material

src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChats} from '../../../../graphql';
 
 @Component({
   selector: 'app-chat-item',
@@ -17,5 +18,5 @@ import {Component, Input} from '@angular/core';
 export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
-  chat: any;
+  chat: GetChats.Chats;
 }

src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {GetChats} from '../../../../graphql';
 
 @Component({
   selector: 'app-chats-list',
@@ -14,7 +15,7 @@ import {Component, Input} from '@angular/core';
 export class ChatsListComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('items')
-  chats: any[];
+  chats: GetChats.Chats[];
 
   constructor() {}
 }

src/app/chats-lister/containers/chats/chats.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ChatsService} from '../../../services/chats.service';
 import {Observable} from 'rxjs';
+import {GetChats} from '../../../../graphql';
 
 @Component({
   template: `
@@ -35,7 +36,7 @@ import {Observable} from 'rxjs';
   styleUrls: ['./chats.component.scss'],
 })
 export class ChatsComponent implements OnInit {
-  chats$: Observable<any[]>;
+  chats$: Observable<GetChats.Chats[]>;
 
   constructor(private chatsService: ChatsService) {
   }

src/app/services/chats.service.ts
@@ -2,6 +2,7 @@ import {map} from 'rxjs/operators';
 import {Apollo} from 'apollo-angular';
 import {Injectable} from '@angular/core';
 import {getChatsQuery} from '../../graphql/getChats.query';
+import {GetChats} from '../../graphql';
 
 @Injectable()
 export class ChatsService {
@@ -10,7 +11,7 @@ export class ChatsService {
   constructor(private apollo: Apollo) {}
 
   getChats() {
-    const query = this.apollo.watchQuery<any>({
+    const query = this.apollo.watchQuery<GetChats.Query>({
       query: getChatsQuery,
       variables: {
         amount: this.messagesAmount,